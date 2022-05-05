3f7d6c8ddd54aba6c7f2778ebd020423a3cb5a16
benny1020 <benny1020@naver.com>
2022-05-05 16:53:58 +0900

    thunder edit for writer

--- router/thunder.py
+++ router/thunder.py
@@ -3,6 +3,7 @@ from model import db_module
 from model import sql_module
 from model import image_module
 from model import thunder_dao
+from model import user_dao
 import json
 from collections import OrderedDict
 from datetime import *
@@ -20,12 +21,59 @@ bp = Blueprint("thunder_bp", __name__, url_prefix='/thunder')
 def test():
     return json.dumps(dumper(thunder_dao.thunderDao().getThunder(1)),ensure_ascii=False)
 
+@bp.route("/join", methods=['POST'])
+def thunder_join():
+    if 'idx' in request.args:
+        idx = request.args.get("idx")
+
+        th_dao = thunder_dao.thunderDao()
+        u_dao = user_dao.UserDao()
+
+        th = th_dao.getThunder(idx)
+        thunder_participants = image_module.db_to_url(th.participants)
+        participants_image = image_module.db_to_url(th.participants_image)
+
+        user_info = u_dao.getUserInfo(idx)
+        user_thunder = image_module.db_to_url(user_info.thunder)
+
+        #이미 참가한 경우
+        if idx in thunder_participants:
+            print("already join")
+            return "400"
+
+        # 정원 초과
+        if th.participants_num == total_num:
+            print("정원 초과")
+            return "400"
+
+        # join
+        th.participants_num = th.participants_num + 1
+        participants_image.append(user_info.profile_image_url)
+        thunder_participants.append(user_info.idx)
+
+        user_thunder.append(idx)
+
+        th_dao.updateThunder(th)
+        u_dao.updateUserInfo(user_info)
+
+
+
+    else:
+        return "400"
+
+
+
+#@bp.route("/cancel", methods=['POST'])
+#def thunder_cancel():
+
+
 #번개 인덱스를 통해 가져오기
 @bp.route("/<int:id>",methods=['GET'])
 def get_thunder(id):
     try:
         return json.dumps(dumper(thunder_dao.thunderDao().getThunder(id)),ensure_ascii=False)
     except Exception as e:
+        print(e)
         return "400"
 
 # thunder filtering
